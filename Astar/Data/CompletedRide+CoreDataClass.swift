cb7e6681c2211ccb0dcbe6e25985cf7465c871ed
Justin Knoetzke <justin.knoetzke@jci.com>
2021-04-21 14:21:34 -0400

    More cleanup of file structure

---
+++ Astar/Data/CompletedRide+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CompletedRide+CoreDataClass.swift
+//  Astar
+//
+//  Created by Justin Knoetzke on 2020-12-11.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CompletedRide)
+public class CompletedRide: NSManagedObject {
+
+}
